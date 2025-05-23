257948a6c4b11efae773c67831c5f4a0a1a015e6
Mitch-Marchand <marchand@vybesoftware.com>
2025-05-22 21:27:00 -0500

    wip

--- gameplay.js
+++ gameplay.js
@@ -160,8 +160,10 @@ router.post("/start", authenticateUser, async (req, res) => {
                 content: `Here's a JSON object containing the information you're expecting: ${JSON.stringify(trimmedMatchData)}.
                 Additionally, here are rules about the game we're playing (name of the game, dollar values, special 
                 rules, handicaps, etc.): "${rules}". Return a JSON object that contains two arrays: one with a list of additionalInputs,
-                each of which contains a question and a list of multiple choice options. the other array should be called
-                scorecards, and should contain an object for each player with the following properties: playerName, tees, chances of
+                each of which contains a question and a list of multiple choice options. don't ask questions about
+                things you can learn from the actual scores, and it's okay if no additional questions are needed for a
+                given game. the other array should be called
+                scorecards, and should contain a scorecard object for each player with the following properties: playerName, tees, chances of
                 winning, winLossBalance, and holes - holes should be an array containing the following information for each hole the
                 golfer is going to play: holeNumber, par, yardage, handicap, strokes (if the rules say the player gets strokes on
                 certain holes), grossScore, and moneyWonLost. Return it as a clean JSON object with no further detail.`
