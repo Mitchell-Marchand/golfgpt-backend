e5cd97b99f61a398f3c0f986bd0b6d6222033df1
Mitch-Marchand <marchand@vybesoftware.com>
2025-06-19 21:57:07 -0500

    wip

--- train/games/scotch.js
+++ train/games/scotch.js
@@ -560,7 +560,7 @@ function getUpdatedHoles(currentScorecard, allAnswers, scores, nameTeams, teams,
 
         console.log("Teams:", nameTeams);
         console.log("Scores:", scores);
-        console.log("Answers:", answers);
+        console.log("Answers:", JSON.stringify(answers, null, 2));
         console.log("First team points", firstTeamPoints);
         console.log("Second team points", secondTeamPoints);
         console.log("First team birdies", firstTeamBirdieCount);
