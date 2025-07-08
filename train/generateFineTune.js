44a99304a4d46ab830f0d74ff4376e8c3b45fed8
Mitch-Marchand <marchand@vybesoftware.com>
2025-07-07 21:13:55 -0500

    wip

--- train/generateFineTune.js
+++ train/generateFineTune.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const mysql = require("mysql2/promise");
-const { countTokensForMessages } = require('./train/utils')
+const { countTokensForMessages } = require('./utils')
 
 require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
 
