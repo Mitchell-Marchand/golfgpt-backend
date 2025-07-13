d1dad647cf0f65e0ccef638d745ca1b9c8ee35dd
Mitch-Marchand <marchand@vybesoftware.com>
2025-07-13 16:37:03 -0500

    wip

--- train/games/scotch.js
+++ train/games/scotch.js
@@ -507,7 +507,7 @@ async function simulateGame(matchId, mariadbPool, summary, builtScorecards, allQ
                 assistantMessageId, assistantResponse
             ]);
 
-            console.log(`ORIGINAL STRING: ${explanation}\nRETURNED (for ${golfer} as name): ${filterGolferResultsInText(explanation, golfer)}\n\n`)
+            //console.log(`ORIGINAL STRING: ${explanation}\nRETURNED (for ${golfer} as name): ${filterGolferResultsInText(explanation, golfer)}\n\n`)
         }
 
         /*messageId = uuidv4();
