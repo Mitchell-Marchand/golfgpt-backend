a9b3cc4e5e08a5b4da4c3f8658c37b5177fcccc8
Mitch-Marchand <marchand@vybesoftware.com>
2025-07-17 16:40:59 -0500

    wip

--- games/config.js
+++ games/config.js
@@ -73,7 +73,7 @@ const junkConfig = `{
 }`;
 
 const scotchConfig = `{
-    teams //an array consisting of the teams, always with each team as one string with player names separared by '&', e.g. ["Player A & Player B", "Player C & "Player D"]. ONLY use the exact names of the golfers provided. If no teams are provided, generate them yourself with the available names
+    teams //an array consisting of the teams, always with each team as one string with player names separared by '&', e.g. ["Player A & Player B", "Player C & "Player D"]. ONLY use the exact names of the golfers provided. If no teams are provided, generate them yourself with the available names. Do NOT ever use "Me" -> only use the EXACT golfer names provided.
     pointVal //number (default is 1). dollar value of points in the match
     points //options are 4, 6, and 8 (default is 4). defines the type of scotch/bridge/umbrella game being played, e.g. 6 point scotch
     autoDoubles //true or false (default is false). whether or not the money increases automatically at any point in the match
@@ -89,7 +89,7 @@ const scotchConfig = `{
 }`;
 
 const vegasConfig = `{
-    teams //an array consisting of the teams, always with each team as one string with player names separared by '&', e.g. ["Player A & Player B", "Player C & "Player D"]. ONLY use the exact names of the golfers provided. If no teams are provided, generate them yourself with the available names
+    teams //an array consisting of the teams, always with each team as one string with player names separared by '&', e.g. ["Player A & Player B", "Player C & "Player D"]. ONLY use the exact names of the golfers provided. If no teams are provided, generate them yourself with the available names. Do NOT ever use "Me" -> only use the EXACT golfer names provided.
     pointVal //number (default is 1). dollar value of points in the match
     autoDoubles //true or false (default is false). whether or not the money increases automatically at any point in the match
     autoDoubleAfterNineTrigger //true or false (default is false). whether or not the money automatically increases after nine holes
