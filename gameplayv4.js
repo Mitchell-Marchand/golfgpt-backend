59631e415478801b9120576d9171373aeb87e220
Mitch-Marchand <marchand@vybesoftware.com>
2025-07-30 15:24:47 -0500

    wip

--- gameplayv4.js
+++ gameplayv4.js
@@ -567,41 +567,43 @@ router.post("/create", authenticateUser, async (req, res) => {
         }
 
         //Push notificiations for follwers of user
-        const [followsRows] = await mariadbPool.query(
-            `SELECT followerId FROM Follows WHERE followedId = ? AND status = 'accepted'`,
-            [req.user.id]
-        );
+        if (isPublic) {
+            const [followsRows] = await mariadbPool.query(
+                `SELECT followerId FROM Follows WHERE followedId = ? AND status = 'accepted'`,
+                [req.user.id]
+            );
 
-        for (let i = 0; i < followsRows.length; i++) {
-            if (golferIds.includes(followsRows[i].followerId)) {
-                continue;
-            }
-            
-            const [rows] = await mariadbPool.query('SELECT expoPushToken FROM Users WHERE id = ?', [followsRows[i].followerId]);
-            if (rows.length === 0) {
-                continue;
-            }
+            for (let i = 0; i < followsRows.length; i++) {
+                if (golferIds.includes(followsRows[i].followerId)) {
+                    continue;
+                }
 
-            const { expoPushToken } = rows[0];
+                const [rows] = await mariadbPool.query('SELECT expoPushToken FROM Users WHERE id = ?', [followsRows[i].followerId]);
+                if (rows.length === 0) {
+                    continue;
+                }
 
-            if (expoPushToken) {
-                const body = {
-                    to: expoPushToken,
-                    sound: 'default',
-                    title: '🚨 New Match Started',
-                    body: `${req.user.firstName} ${req.user.lastName} just started a game of ${raw}. Follow the action!`,
-                };
+                const { expoPushToken } = rows[0];
 
-                const response = await fetch('https://exp.host/--/api/v2/push/send', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(body),
-                });
+                if (expoPushToken) {
+                    const body = {
+                        to: expoPushToken,
+                        sound: 'default',
+                        title: '🚨 New Match Started',
+                        body: `${req.user.firstName} ${req.user.lastName} just started a game of ${raw}. Follow the action!`,
+                    };
 
-                const result = await response.json();
-                console.log('Expo Push Response:', result);
-            } else {
-                console.log("No push token");
+                    const response = await fetch('https://exp.host/--/api/v2/push/send', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(body),
+                    });
+
+                    const result = await response.json();
+                    console.log('Expo Push Response:', result);
+                } else {
+                    console.log("No push token");
+                }
             }
         }
 
