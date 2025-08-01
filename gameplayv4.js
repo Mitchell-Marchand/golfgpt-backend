8b94eef1ad387577b353187d8becc0e6f07782a8
Mitch-Marchand <marchand@vybesoftware.com>
2025-08-01 10:33:02 -0500

    wip

--- gameplayv4.js
+++ gameplayv4.js
@@ -884,7 +884,7 @@ router.get("/matches", authenticateUser, async (req, res) => {
 
     try {
         const [rows] = await mariadbPool.query(
-            `SELECT m.id, m.displayName, m.golfers, m.status, m.summary, m.teeTime, m.courseId,
+            `SELECT m.id, m.displayName, m.golfers, m.status, m.summary, m.teeTime, m.courseId, m.createdBy
                     c.courseId AS courseId, c.courseName AS courseName
              FROM Matches m
              LEFT JOIN Courses c ON m.courseId = c.courseId
@@ -910,6 +910,7 @@ router.get("/matches", authenticateUser, async (req, res) => {
             //config: match.config ? JSON.parse(match.config) : {},
             //junkConfig: match.strippedJunk ? JSON.parse(match.strippedJunk) : {},
             //isPublic: match.isPublic,
+            createdBy: match.createdBy,
             summary: match.summary,
             status: match.status,
             teeTime: match.teeTime,
