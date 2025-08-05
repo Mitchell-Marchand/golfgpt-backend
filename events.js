617c8fb0dd040fcd2df204cb8168e56e3997148f
Mitch-Marchand <marchand@vybesoftware.com>
2025-08-05 07:26:11 -0500

    wip

--- events.js
+++ events.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql2/promise');
 const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
 
