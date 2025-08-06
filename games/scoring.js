fd142f10a96e644a7ec6a8dd277a957c96d907f5
Mitch-Marchand <marchand@vybesoftware.com>
2025-08-06 08:36:23 -0500

    wip

--- games/scoring.js
+++ games/scoring.js
@@ -306,7 +306,7 @@ function junk(scorecards, answers, strippedJunk, golfers, teams) {
         scorecards = trackSkins(scorecards, strippedJunk.skins, golfers);
     }
 
-    if (strippedJunk.birdieStreak?.valid) {
+    /*if (strippedJunk.birdieStreak?.valid) {
         scorecards = trackStreaks(scorecards, strippedJunk.birdieStreak, -1)
     }
 
@@ -316,7 +316,7 @@ function junk(scorecards, answers, strippedJunk, golfers, teams) {
 
     if (strippedJunk.bogeyStreak?.valid) {
         scorecards = trackStreaks(scorecards, strippedJunk.bogeyStreak, 1)
-    }
+    }*/
 
     console.log("scorecards length ended", scorecards?.length);
 
