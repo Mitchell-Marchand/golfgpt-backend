274985164c873a89f84444e54b28d9afbc306b1d
Mitch-Marchand <marchand@vybesoftware.com>
2025-08-06 08:46:25 -0500

    wip

--- gameplayv4.js
+++ gameplayv4.js
@@ -828,12 +828,7 @@ router.post("/score/submit", authenticateUser, async (req, res) => {
             )
         }
 
-        console.log("scorecards", JSON.stringify(scorecards));
-
         scorecards = junk(scorecards, answers, strippedJunk, golfers, config.teams || false);
-        scorecards = calculateWinPercents(scorecards);
-
-        //console.log("scorecards", JSON.stringify(scorecards));
 
         let allHolesPlayed = true;
         for (i = 0; i < scorecards.length; i++) {
@@ -868,6 +863,7 @@ router.post("/score/submit", authenticateUser, async (req, res) => {
             status = "COMPLETED";
         }
 
+        scorecards = calculateWinPercents(scorecards);
         const summary = generateSummary(scorecards, configType, config);
 
         await mariadbPool.query(
